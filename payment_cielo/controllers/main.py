0425f2b08c2f0596796c2d14765300378b731705
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-06-27 19:54:20 -0300

    Terminado parte de enviar dados via post

--- payment_cielo/controllers/main.py
+++ payment_cielo/controllers/main.py
@@ -18,3 +18,49 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+try:
+    import simplejson as json
+except ImportError:
+    import json
+import logging
+import pprint
+import urllib2
+import werkzeug
+
+from openerp import http, SUPERUSER_ID
+from openerp.http import request
+
+_logger = logging.getLogger(__name__)
+
+
+class CieloController(http.Controller):
+    _return_url = '/cielo/retorno/'
+    _notify_url = '/cielo/notificacao/'
+    _status_url = '/cielo/status/'
+
+    def cielo_validate_data(self, **post):
+        pass
+
+    @http.route('/cielo/retorno/', type='http', auth='none', methods=['POST'])
+    def cielo_retorno(self, **post):
+        """ Paypal IPN. """
+        _logger.info('Beginning Paypal IPN form_feedback with post data %s', pprint.pformat(post))  # debug
+        self.cielo_validate_data(**post)
+        return ''
+
+    @http.route('/cielo/notificacao/', type='http', auth="none", methods=['POST'])
+    def cielo_notify(self, **post):
+        """ Paypal DPN """
+        _logger.info('Beginning Paypal DPN form_feedback with post data %s', pprint.pformat(post))  # debug
+        self.cielo_validate_data(**post)
+        return werkzeug.utils.redirect(return_url)
+
+    @http.route('/cielo/status/', type='http', auth="none")
+    def cielo_cancel(self, **post):
+        """ When the user cancels its Paypal payment: GET on this route """
+        cr, uid, context = request.cr, SUPERUSER_ID, request.context
+        _logger.info('Beginning Paypal cancel with post data %s', pprint.pformat(post))  # debug
+        return_url = self._get_return_url(**post)
+        return werkzeug.utils.redirect(return_url)
+
+
