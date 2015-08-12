75cb88d174ac5821507b46fe9408548c3de54316
Danimar Ribeiro <danimaribeiro@gmail.com>
2015-08-12 18:26:35 -0300

    Fix pep errors for build

--- trust_base/models/account_invoice.py
+++ trust_base/models/account_invoice.py
@@ -17,7 +17,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
 #                                                                             #
 ###############################################################################
-from openerp import models, fields, api
+
+from openerp import models
 
 
 class AccountInvoice(models.Model):
