8b443e0e753d6c1c78e57f2337bb23af165ec40c
Mackilem Van der Laan <mack.vdl@gmail.com>
2015-11-06 16:41:34 -0200

    corrigido para a api v8

--- trust_task_time_control/models/project_task_type.py
+++ trust_task_time_control/models/project_task_type.py
@@ -4,12 +4,9 @@ Created on Nov 5, 2015
 @author: developer
 '''
 from openerp import api, fields, models
-from openerp.osv import fields, osv
+
 
 class ProjectTaskType(models.Model):
     _inherit = 'project.task.type'
 
-    _columns = {
-        'count_time': fields.boolean('Count Time'),
-        #'active': fields.Boolean('Active'),
-        }
+    count_time = fields.Boolean('Count Time')
