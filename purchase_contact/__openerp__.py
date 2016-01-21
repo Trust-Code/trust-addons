c1dcceeecbd14bfc0fd66ee2da109483f7464a07
Mackilem Van der Laan <mack.vdl@gmail.com>
2016-01-21 11:52:37 -0200

    novo modulo para selecionar o contato na compra

---
+++ purchase_contact/__openerp__.py
@@ -0,0 +1,33 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2015 TrustCode - www.trustcode.com.br                         #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
+{
+    'name': 'Purchase Contact',
+    'summary': 'Insere um campo para selecionar o contato da compra',
+    'category': 'Trust-Code',
+    'license': 'AGPL-3',
+    'author': 'Trustcode Tecnologia Ltda',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': ['Mackilem Van der Laan Soares <mack.vdl@gmail.com>',
+                     'Danimar Ribeiro <danimaribeiro@gmail.com>'
+                     ],
+    'version': '1.0',
+    'depends': ['purchase', ],
+    'data': ['views/purchase_contact_view.xml', ],
+    'installable': True,
+}
---
+++ purchase_contact/models/purchase_contact.py
@@ -0,0 +1,35 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2015 TrustCode - www.trustcode.com.br                         #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
+
+from openerp import api, fields, models
+
+
+class PurchaseContact(models.Model):
+    _inherit = 'purchase.order'
+
+    partner_contact_id = fields.Many2one(
+        'res.partner',
+        'partner_id',
+        domain="[('parent_id','=',partner_id),]",
+        readonly=True,
+        required=True,
+        states={
+            'draft': [('readonly', False)],
+            'sent': [('readonly', False)]},
+        help="Insert here the purchase contact.")
