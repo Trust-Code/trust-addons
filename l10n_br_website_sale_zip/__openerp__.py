f5fbb867ee5c38ee3770101943f5cfa0d6a1e202
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-05-05 19:09:06 -0300

    Novo módulo que implementa pesquisa de cep no formulário de e-commerce

---
+++ l10n_br_website_sale_zip/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Website Sale Checkout Zip Search for Brazil',
+    'summary': """Adds zip search to e-commerce checkout""",
+    'version': '1.0',
+    'category': 'Website',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>'
+    ],
+    'depends': [
+        'l10n_br_zip',
+    ],
+    'data': [
+        'views/website_sale_view.xml',
+    ],
+}
