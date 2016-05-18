1c3ae2a7ccefe635f8343043b348e5e66187e1a5
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-05-18 11:56:39 -0300

    Galeria de imagens para o produto no e-commerce

---
+++ website_sale_multi_image_zoom/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Multi Images in Website',
+    'summary': """Implement multi images to shop in products""",
+    'version': '8.0.1.0.0',
+    'category': 'website',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>'
+    ],
+    'depends': [
+        'product_multi_image',
+    ],
+    'data': [
+        'views/product_templates.xml',
+    ],
+}
---
+++ website_sale_multi_image_zoom/static/src/js/website_sale_multi_image_zoom.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+   $('.sp-wrap').smoothproducts();
+});
