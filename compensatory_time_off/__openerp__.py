46d4424565c24896af3163bab9ba16409d05e62a
Mackilem <mack.vdl@gmail.com>
2016-07-05 17:27:59 -0300

    primeiro commit do módulo

---
+++ compensatory_time_off/__openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# Copyright (C) 2015 Trustcode - www.trustcode.com.br                         #
+#                                                                             #
+# This program is free software: you can redistribute it and/or modify        #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation, either version 3 of the License, or           #
+# (at your option) any later version.                                         #
+#                                                                             #
+# This program is distributed in the hope that it will be useful,             #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of              #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               #
+# GNU General Public License for more details.                                #
+#                                                                             #
+# You should have received a copy of the GNU General Public License           #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.       #
+###############################################################################
+
+{
+    'name': 'Compensatory Time Off - Trustcode',
+    'summary': """Compensatory Time Off for HR""",
+    'version': '8.0',
+    'category': 'Trustcode',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': ['Danimar Ribeiro <danimaribeiro@gmail.com>',
+                     'Mackilem Van der Laan Soares <mack.vdl@gmail.com>'
+                     ],
+    'depends': [
+        'hr_attendance',
+	'hr_timesheet_sheet',
+        'resource',
+    ],
+    'data': [
+        'views/timesheet_comptime_view.xml',
+    ],
+    'application': True,
+    'auto_install': False
+}
