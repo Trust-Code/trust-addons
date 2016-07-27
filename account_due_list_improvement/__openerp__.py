6e502132a8b6d3d970a7fef1dc327c14fe3ecab9
Danimar Ribeiro <danimaribeiro@gmail.com>
2016-07-27 10:27:48 -0300

    Módulo de melhoria no account due list

---
+++ account_due_list_improvement/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Account Due List Improvements',
+    'summary': """Melhor a visão de pagamentos e vencimentos""",
+    'version': '8.0.1.0.0',
+    'category': 'Website',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>'
+    ],
+    'depends': [
+        'account_due_list',
+    ],
+    'data': [
+        'views/account_due_list.xml',
+    ],
+}
---
+++ account_due_list_improvement/models/account_move_line.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# © 2016 Danimar Ribeiro, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import api, fields, models
+
+
+class AccountMoveLine(models.Model):
+    _inherit = 'account.move.line'
+
+    @api.multi
+    @api.depends('debit', 'credit', 'payment_type', 'maturity_residual')
+    def _compute_amounts(self):
+        for item in self:
+            amount = item.debit if item.payment_type == 'receivable' \
+                else item.credit
+            item.amount_to_pay = amount
+            item.amount_paid = item.amount_to_pay - item.maturity_residual
+            if item.payment_type == 'payable':
+                item.amount_paid *= -1
+
+    amount_to_pay = fields.Float(strifffng="A Pagar/Receber",
+                                 compute="_compute_amounts")
+    amount_paid = fields.Float(string="Pago/Recebido",
+                               compute="_compute_amounts")
+
+    payment_type = fields.Selection(
+        string="Tipo",
+        related="account_id.type"
+    )
+
+    @api.multi
+    def action_redirect_to_payment(self):
+        self.ensure_one()
+        if self.payment_type == 'receivable':
+            dummy, action_id = self.env['ir.model.data'].get_object_reference(
+                'account_voucher', 'action_vendor_receipt')
+            context = "{'type':'receipt', 'default_partner_id': %s, \
+                'default_amount': %d}" % (self.partner_id.id,
+                                          self.maturity_residual)
+        else:
+            dummy, action_id = self.env['ir.model.data'].get_object_reference(
+                'account_voucher', 'action_vendor_payment')
+            context = "{'type':'payment', 'default_partner_id': %s, \
+                'default_amount': %d}" % (self.partner_id.id,
+                                          self.maturity_residual*-1)
+        vals = self.env['ir.actions.act_window'].browse(action_id).read()[0]
+        vals['context'] = context
+        vals['views'] = [vals['views'][1], vals['views'][0]]
+        return vals
