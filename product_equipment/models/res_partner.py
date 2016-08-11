c10e15609c636ff6128bee1e668b1189bb00f1df
martini97 <alessandrofmartini@gmail.com>
2016-08-11 20:40:30 -0300

    Criação do módulo como descrito na tarefa Contador de Tempo em Suporte

--- product_equipment/models/res_partner.py
+++ product_equipment/models/res_partner.py
@@ -8,7 +8,7 @@ from openerp import fields, models
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    equipment_ids = fields.One2many('product.product', 'partner_id',
+    equipment_ids = fields.One2many('product.template', 'partner_id',
                                     string="Equipamentos")
 
     web_name = fields.Char(max_length=30, string="Nome da Rede")
