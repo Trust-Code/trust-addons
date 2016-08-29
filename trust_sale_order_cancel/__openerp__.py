6a2c9ee2755778e2a7524e28a78607b3bc828ec8
martini97 <alessandrofmartini@gmail.com>
2016-08-29 15:35:00 -0300

    Módulo que permite cancelar parcialmente o pedido

---
+++ trust_sale_order_cancel/__openerp__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Alessandro Fernandes Martini, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': "Trust Sale Order Cancel",
+    'summary': """Cria um botão para cancelar parcialmente produtos dos \
+pedidos""",
+    'version': '8.0.1.0',
+    'category': 'MRP',
+    'author': 'Trustcode',
+    'license': 'AGPL-3',
+    'website': 'http://www.trustcode.com.br',
+    'contributors': ['Mackilem Van der Laan Soares <mack.vdl@gmail.com>',
+                     'Danimar Ribeiro <danimaribeiro@gmail.com>',
+                     'Alessandro Fernandes Martini \
+<alessandrofmartini@gmail.com>'
+                     'Jenifer Rocha <@.com>',
+                     'Marcos Abreu <@.com>'
+                     ],
+    'depends': [
+        'sale', 'procurement',
+    ],
+    'data': [
+        'views/trust_sale_order_cancel.xml',
+        'wizard/sale_order_cancel_action.xml'
+    ],
+    'application': True,
+}
---
+++ trust_sale_order_cancel/models/sale_order_line.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# © 2016 Alessandro Fernandes Martini, Trustcode
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from openerp import api, models
+from openerp.exceptions import Warning as UserError
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    @api.multi
+    def call_cancel_wizard(self):
+        if self.state != 'draft':
+            raise UserError(
+                'Movimentação Proibida!',
+                'Esta linha do pedido já foi cancelada ou concluída!')
+
+        return({
+            'name': 'sale_order_cancel_action',
+            'type': 'ir.actions.act_window',
+            'res_model': 'sale.order.line.cancel',
+            'view_type': 'form',
+            'view_mode': 'form',
+            'target': 'new',
+            'context': {'default_sale_order_line_id': self.id,
+                        'default_invoice_amount': self.product_uom_qty}
+        })
