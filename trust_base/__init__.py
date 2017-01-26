1a4754a8fd92114b148f14718b4ec8170a106f4d
Alessandro Martini <alessandrofmartini@gmail.com>
2017-01-26 16:32:04 -0200

    Monkeypatch para converter o horario de utc para a timezone do cliente ao exportar dados para CSV

--- trust_base/__init__.py
+++ trust_base/__init__.py
@@ -18,3 +18,23 @@
 ###############################################################################
 
 from . import controllers
+
+import pytz
+
+from openerp.fields import Datetime
+from openerp.tools import ustr
+
+
+def convert_to_export(self, value, env):
+    """Monkeypatch para converter timezone de UTC para a do cliente"""
+    if not value:
+        return ''
+    br_timezone = pytz.timezone(env.context.get('tz', u'America/Sao_Paulo'))
+    datetime_as_utc = pytz.utc.localize(self.from_string(value), is_dst=None)
+    datetime_as_br = datetime_as_utc.astimezone(br_timezone)
+    value = self.to_string(datetime_as_br)
+    return self.from_string(value) if env.context.get('export_raw_data') \
+        else ustr(value)
+
+
+Datetime.convert_to_export = convert_to_export
